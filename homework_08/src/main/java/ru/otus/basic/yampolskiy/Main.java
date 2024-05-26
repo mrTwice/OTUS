847755a4725c4aee34870bd330f0127f9553ba05
Igor Yampolskiy <clubber_77@mail.ru>
2024-05-26 15:07:30 +0300

    Homework_08 fixed

--- homework_08/src/main/java/ru/otus/basic/yampolskiy/Main.java
+++ homework_08/src/main/java/ru/otus/basic/yampolskiy/Main.java
@@ -7,34 +7,30 @@ import ru.otus.basic.yampolskiy.exeptions.AppArraySizeException;
 public class Main {
 
     static final String[][] FIRST_ARRAY = {
-            {"1", "1"},
-            {"a", "-1"}
+            {"1", "1", "1", "1"},
+            {"a", "-1", "1", "1"}
     };
 
     static final String[][] SECOND_ARRAY = {
-            {"1", "1"},
-            {"a"}
+            {"1", "1", "1", "1"},
+            {"a", "1", "1"}
     };
 
     public static void main(String[] args) {
-
-
-
         try {
-            System.out.println(convertStringToIntAndSumAllElements(FIRST_ARRAY));
+            System.out.println(convertStringToIntAndSumAllElements(SECOND_ARRAY));
         } catch (AppArraySizeException | AppArrayDataException e) {
             System.out.println(e.getMessage());
         }
-
     }
 
     public static int convertStringToIntAndSumAllElements(String[][] array) throws AppArraySizeException, AppArrayDataException {
-        if(array[0].length != 2 || array[1].length != 2) {
+        if (array[0].length != 4 || array[1].length != 4) {
             throw new AppArraySizeException(array);
         }
         int sum = 0;
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[i].length; j++){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 try {
                     int currentElement = Integer.parseInt(array[i][j]);
                     sum += currentElement;
