d17f437faa9545668aa04d9a49f2f1eb54ddac5d
Igor Yampolskiy <clubber_77@mail.ru>
2024-05-26 22:29:52 +0300

    Homework_08 some fixes

--- homework_08/src/main/java/ru/otus/basic/yampolskiy/exeptions/AppArrayDataException.java
+++ homework_08/src/main/java/ru/otus/basic/yampolskiy/exeptions/AppArrayDataException.java
@@ -2,14 +2,15 @@ package ru.otus.basic.yampolskiy.exeptions;
 
 public class AppArrayDataException extends Exception{
 
-    private int i;
-    private int j;
+    private final int i;
+    private final int j;
 
     public AppArrayDataException(int i, int j) {
         this.i = i;
         this.j = j;
     }
 
+    @Override
     public String getMessage() {
         return "Не удалось преобразовать элемент с индексом"+  " [" + i + "," + j + "]"+" в целое число.";
     }
--- homework_08/src/main/java/ru/otus/basic/yampolskiy/exeptions/AppArraySizeException.java
+++ homework_08/src/main/java/ru/otus/basic/yampolskiy/exeptions/AppArraySizeException.java
@@ -2,14 +2,15 @@ package ru.otus.basic.yampolskiy.exeptions;
 
 public class AppArraySizeException extends Exception{
 
-    private int value;
-    private String message;
+    private final int value;
+    private final String message;
 
     public AppArraySizeException( int value, String message) {
         this.value = value;
         this.message = message;
     }
 
+    @Override
     public String getMessage() {
         return message + value;
     }
