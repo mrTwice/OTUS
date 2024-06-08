3627a396b01b0936cd2c122e8372bf4df849e142
Igor Yampolskiy <clubber_77@mail.ru>
2024-06-08 11:48:51 +0300

    - add module homework_13. init commit

--- homework_13/src/main/java/ru/otus/basic/yampolskiy/Main.java
+++ homework_13/src/main/java/ru/otus/basic/yampolskiy/Main.java
@@ -1,9 +1,7 @@
 package ru.otus.basic.yampolskiy;
 
-
 public class Main {
     public static void main(String[] args) {
-        Presenter presenter = new Presenter(new PhoneBook(), new View());
-        presenter.create();
+        System.out.println("Hello world!");
     }
 }
