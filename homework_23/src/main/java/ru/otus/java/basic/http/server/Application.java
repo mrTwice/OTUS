f191e56520227daa9d725f29fe29d7a03107b93b
mrTwice <yampolskiy86@gmail.com>
2024-07-28 14:47:20 +0300

    add code from lesson

--- homework_23/src/main/java/ru/otus/java/basic/http/server/Application.java
+++ homework_23/src/main/java/ru/otus/java/basic/http/server/Application.java
@@ -1,6 +1,15 @@
 package ru.otus.java.basic.http.server;
 
 public class Application {
+    /*
+
+    Домашнее задание:
+    - Избавиться от System.out.println, перейти на использование логирования
+    - Сделайте парсинг заголовков запроса в Map<String, String>
+    - * Добавьте возможность удалять продукты через DELETE /items?id=10 (т.е. сервер должен удалить продукт с ид=10)
+
+     */
+
     public static void main(String[] args) {
         new HttpServer(8189).start();
     }
